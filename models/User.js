d1a7a4d9132f07d435252441927422a7062dc0c3
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-18 23:42:30 +0530

    feat: add user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+// user schema
+
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+        ],
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+module.exports = User = mongoose.model('user', UserSchema)
