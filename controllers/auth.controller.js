615ffa316b112e138a34f62b8cda93cc4d72e27e
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-19 11:07:32 +0530

    :package: add bcryptjs and jsonwebtoken

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,40 @@
 
 
+
+
+// register user
+// POST /api/signup
+// public
+
 const signup = async (req, res) => {
+
+     const { username, email, password } = req.body;
+
+    try {
+
+        //check if user exists
+        //if yes send error
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json('User already exists');
+        }
+
+        //if no 
+
+        user = new User({
+            username,
+            email,
+            password
+        })
+
+
+       
+
+    } catch (err) {
+        console.log(err)
+        res.status(400).json('server error')
+    }
+
   res.json("sign up user");
 };
 
