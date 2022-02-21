5bc932e3b6c348dc0c5e7574fe80c75ff1ce5d6f
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-21 21:14:04 +0530

    :pencil2: fix typo in index file

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const authRouter = require("./routes/auth.router");
 const userRouter = require("./routes/users.router");
 const auth = require('./middleware/auth');
 
-
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 
 
@@ -23,17 +22,13 @@ app.use("/api/auth", authRouter);
 
 app.get('/', (req, res)=>res.send('API running'))
 
-const PORT = process.env.PORT || 5000;
 
-
-
-const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URI);
-    app.listen(port, () => {
-      console.log(`Server listening on port: ${port}`);
+    app.listen(PORT, () => {
+      console.log(`Server listening on port: ${PORT}`);
     });
   } catch (error) {
     console.log(error);
